345b34f566c7ba0543ec5a51bb5584e92b47db04
BaackPro <btece08@gmail.com>
2025-08-30 10:26:00 +0100

    Auto-update 30/08/2025 10:26

---
+++ push.ps1
@@ -0,0 +1,28 @@
+
+cd "C:\btece-formation"
+git add --all
+git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
+git push origin main
+
+
+git push origin HEAD
+
+Santanda
+
+
+
+cd "C:\Users\prince CHABI\Desktop\btece"
+# D'abord récupérer les changements distants
+git pull origin main
+# Ensuite ajouter vos modifications
+git add --all
+# Créer un commit
+git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
+# Finalement pousser vers GitHub
+git push origin main
+
+
+cd "C:\Users\prince CHABI\Desktop\btece"
+git add --all
+git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
+git push origin main
